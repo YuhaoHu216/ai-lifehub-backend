52ab0dd405a421da74399d96495e827b52274ff6
HYH <huyuhao612@outlook.com>
2025-10-03 23:11:41 +0800

    项目创建

---
+++ src/main/java/space/huyuhao/AiLifehubApplication.java
@@ -0,0 +1,13 @@
+package space.huyuhao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiLifehubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiLifehubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/space/huyuhao/AiLifehubApplicationTests.java
@@ -0,0 +1,13 @@
+package space.huyuhao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiLifehubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
