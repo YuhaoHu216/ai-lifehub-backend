f84ecebf9f0c18e55253d2db7b04871a0a0cd128
HYH <huyuhao612@outlook.com>
2025-10-10 23:57:26 +0800

    导入用逻辑过期解决缓存击穿代码

--- src/main/java/space/huyuhao/service/impl/ShopServiceImpl.java
+++ src/main/java/space/huyuhao/service/impl/ShopServiceImpl.java
@@ -3,6 +3,7 @@ package space.huyuhao.service.impl;
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.util.BooleanUtil;
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -13,8 +14,12 @@ import space.huyuhao.exception.MyException;
 import space.huyuhao.mapper.ShopMapper;
 import space.huyuhao.po.Shop;
 import space.huyuhao.service.ShopService;
+import space.huyuhao.utils.RedisData;
 import space.huyuhao.vo.Result;
 
+import java.time.LocalDateTime;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 import static space.huyuhao.constant.RedisConstants.*;
@@ -114,6 +119,62 @@ public class ShopServiceImpl implements ShopService {
         }
         return shop;
     }
+
+    // 用于预热缓存
+    public void saveShop2Redis(Long id,Long expireSeconds){
+        //1.查询店铺数据
+        Shop shop = shopMapper.selectShopById(id);
+        //2.封装逻辑过期时间
+        RedisData redisData = new RedisData();
+        redisData.setData(shop);
+        redisData.setExpireTime(LocalDateTime.now().plusSeconds(expireSeconds));
+        //3.写入Redis
+        stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY + id,JSONUtil.toJsonStr(redisData));
+    }
+
+    // 逻辑过期解决缓存击穿问题 TODO 自行实现逻辑过期解决缓存击穿问题
+    private static final ExecutorService CACHE_REBUILD_EXECUTOR = Executors.newFixedThreadPool(10); //创建线程池用于缓存重建
+    public Shop queryWithLogicalExpire( Long id ) {
+        String key = CACHE_SHOP_KEY + id;
+        // 1.从redis查询商铺缓存
+        String json = stringRedisTemplate.opsForValue().get(key);
+        // 2.判断是否存在
+        if (StrUtil.isBlank(json)) {
+            // 3.存在，直接返回
+            return null;
+        }
+        // 4.命中，需要先把json反序列化为对象
+        RedisData redisData = JSONUtil.toBean(json, RedisData.class);
+        Shop shop = JSONUtil.toBean((JSONObject) redisData.getData(), Shop.class);
+        LocalDateTime expireTime = redisData.getExpireTime();
+        // 5.判断是否过期
+        if(expireTime.isAfter(LocalDateTime.now())) {
+            // 5.1.未过期，直接返回店铺信息
+            return shop;
+        }
+        // 5.2.已过期，需要缓存重建
+        // 6.缓存重建
+        // 6.1.获取互斥锁
+        String lockKey = LOCK_SHOP_KEY + id;
+        boolean isLock = tryLock(lockKey);
+        // 6.2.判断是否获取锁成功
+        if (isLock){
+            CACHE_REBUILD_EXECUTOR.submit( ()->{
+
+                try{
+                    //重建缓存
+                    this.saveShop2Redis(id,20L);
+                }catch (Exception e){
+                    throw new RuntimeException(e);
+                }finally {
+                    unlock(lockKey);
+                }
+            });
+        }
+        // 6.4.返回过期的商铺信息
+        return shop;
+    }
+
 
     // 获取锁
     public boolean tryLock(String key){
--- src/test/java/space/huyuhao/AiLifehubApplicationTests.java
+++ src/test/java/space/huyuhao/AiLifehubApplicationTests.java
@@ -1,13 +1,25 @@
 package space.huyuhao;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import space.huyuhao.service.impl.ShopServiceImpl;
 
 @SpringBootTest
 class AiLifehubApplicationTests {
 
+    @Autowired
+    private ShopServiceImpl shopServiceImpl;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void testSaveShop(){
+        for(long i = 1; i < 10; i++){
+            shopServiceImpl.savaShop2Redis(i,10L);
+        }
     }
 
 }
