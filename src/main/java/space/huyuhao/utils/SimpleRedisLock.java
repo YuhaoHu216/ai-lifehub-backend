f9cc3b14d45e9827d0b6b2d00a41ac3108c8b708
HYH <huyuhao612@outlook.com>
2025-11-01 23:45:50 +0800

    用lua脚本解决解锁时的误删锁情况

--- src/main/java/space/huyuhao/utils/SimpleRedisLock.java
+++ src/main/java/space/huyuhao/utils/SimpleRedisLock.java
@@ -1,10 +1,13 @@
 package space.huyuhao.utils;
 
 import cn.hutool.core.lang.UUID;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
 
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 public class SimpleRedisLock implements Lock{
@@ -15,6 +18,14 @@ public class SimpleRedisLock implements Lock{
     private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
 
     private StringRedisTemplate stringRedisTemplate;
+
+    // 初始化调用lua脚本
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
 
     public SimpleRedisLock(String name,StringRedisTemplate stringRedisTemplate){
         this.name = name;
@@ -33,13 +44,18 @@ public class SimpleRedisLock implements Lock{
 
     @Override
     public void unLock() {
-        // 获取线程标识
-        String threadId = ID_PREFIX + Thread.currentThread().getId();
-        // 获取锁中的线程标识
-        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
-        // 比较后再解锁
-        if(threadId.equals(id)){
-            stringRedisTemplate.delete(KEY_PREFIX + name);
-        }
+//        // 获取线程标识
+//        String threadId = ID_PREFIX + Thread.currentThread().getId();
+//        // 获取锁中的线程标识
+//        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
+//        // 比较后再解锁
+//        if(threadId.equals(id)){
+//            stringRedisTemplate.delete(KEY_PREFIX + name);
+//        }
+        // 调用lua脚本
+        stringRedisTemplate.execute(
+                UNLOCK_SCRIPT,
+                Collections.singletonList(KEY_PREFIX + name),
+                ID_PREFIX + Thread.currentThread().getId());
     }
 }
